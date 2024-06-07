9bbbf6170ac59a7cb860fe45b8250c34468adce3
Jenya476 <jenya476@gmail.com>
2024-06-07 16:32:35 +0300

    Added log

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import ResponsiveDrawer from './components/ResponsiveDrawer';
 
 function App() {
-  return (
-    <div className="App">
-      <ResponsiveDrawer/>
-    </div>
-  );
+    console.log('Allbridge Core SDK Example Project');
+    return (
+        <div className="App">
+            <ResponsiveDrawer/>
+        </div>
+    );
 }
 
 export default App;
